344a4697dc100dfcd9f52dc1657876fcdb2da321
d093w1z <d093w1z@protonmail.com>
2025-07-29 01:29:53 +0530

    logging update: added separate logs for game and lib defined macros to use for logging

--- src/game/atcos.cpp
+++ src/game/atcos.cpp
@@ -4,6 +4,7 @@
 
 #include <SFML/Graphics.hpp>
 
+#include "core/core.hpp"
 #include "log.hpp"
 
 ATCOSApp::ATCOSApp()
@@ -13,6 +14,7 @@ ATCOSApp::ATCOSApp()
     auto window = sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "CMake SFML Project");
     window.setFramerateLimit(144);
     window.setPosition(sf::Vector2i(100, 100));
+    ATCOS_INFO("Started application");
 
     while (window.isOpen())
     {
@@ -21,7 +23,7 @@ ATCOSApp::ATCOSApp()
             if (event->is<sf::Event::Closed>())
             {
                 window.close();
-                Log::GetLogger()->info("Window closed!");
+                ATCOS_INFO("Window closed");
             }
         }
 
--- src/utils/log.hpp
+++ src/utils/log.hpp
@@ -4,8 +4,26 @@ class Log
 {
    public:
     static void Init();
-    inline static std::shared_ptr<spdlog::logger>& GetLogger() { return sLogger; };
+    inline static std::shared_ptr<spdlog::logger>& GetGameLogger() { return sGameLogger; };
+    inline static std::shared_ptr<spdlog::logger>& GetLibLogger() { return sLibLogger; };
 
    private:
-    static std::shared_ptr<spdlog::logger> sLogger;
+    static std::shared_ptr<spdlog::logger> sGameLogger;
+    static std::shared_ptr<spdlog::logger> sLibLogger;
 };
+
+#define ATCOS_WARN(message) Log::GetGameLogger()->warn(message);
+#define ATCOS_DEBUG(message) Log::GetGameLogger()->debug(message);
+#define ATCOS_TRACE(message) Log::GetGameLogger()->trace(message);
+#define ATCOS_ERROR(message) Log::GetGameLogger()->error(message);
+#define ATCOS_INFO(message) Log::GetGameLogger()->info(message);
+#define ATCOS_CRITICAL(message) Log::GetGameLogger()->critical(message);
+#define ATCOS_LOG(level, message) Log::GetGameLogger()->log(level, message);
+
+#define ATCOS_LIB_WARN(message) Log::GetLibLogger()->warn(message);
+#define ATCOS_LIB_DEBUG(message) Log::GetLibLogger()->debug(message);
+#define ATCOS_LIB_TRACE(message) Log::GetLibLogger()->trace(message);
+#define ATCOS_LIB_ERROR(message) Log::GetLibLogger()->error(message);
+#define ATCOS_LIB_INFO(message) Log::GetLibLogger()->info(message);
+#define ATCOS_LIB_CRITICAL(message) Log::GetLibLogger()->critical(message);
+#define ATCOS_LIB_LOG(level, message) Log::GetLibLogger()->log(level, message);
